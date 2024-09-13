163be0bb106155301babd0cae73bec714970af49
uncroos <any492266@gmail.com>
2024-09-13 22:21:14 +0900

    init

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,6 @@ module bapjwo-backend
 
 go 1.23.1
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require github.com/rs/cors v1.11.1 // indirect
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,6 +1,11 @@
 document.getElementById("fetch-meal").addEventListener("click", () => {
   fetch("http://localhost:8080/meal")
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
     .then((data) => {
       const mealInfo = document.getElementById("meal-info");
       mealInfo.innerHTML = "";
